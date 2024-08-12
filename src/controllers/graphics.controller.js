3769dca1afc2366a0535a558a368a81120996a36
danielpresamarmolejo <137099929+danielpresamarmolejo@users.noreply.github.com>
2024-08-12 10:46:14 -0600

    Update graphics.controller.js

--- src/controllers/graphics.controller.js
+++ src/controllers/graphics.controller.js
@@ -9,52 +9,39 @@ import jwt from "jsonwebtoken";
 export const graphics2 = async (req, res) => {
     try {
         const careers = await Carrer.find();
-        console.log("Carreras encontradas:", careers);
 
         const subjects = await allSubjects();
-        console.log("Calificaciones encontradas:", subjects);
 
         const careerCounts = {};
 
         careers.forEach(career => {
-            console.log(`\nAnalizando carrera: ${career.name} (ID: ${career._id})`);
-
             const studentsInCareer = subjects.filter(subject => {
                 const isMatch = subject.grade.some(grade => {
                     const careerIds = grade.career.map(id => id.toString());
                     const currentCareerId = career._id.toString();
-                    console.log(`Comparando ID de carrera en calificación: ${careerIds} con ID de carrera actual: ${currentCareerId}`);
+                   
                     return careerIds.includes(currentCareerId);
                 });
                 return isMatch;
             });
 
-            console.log(`Estudiantes en ${career.name}:`, studentsInCareer);
+           
 
             const uniqueStudents = new Set(studentsInCareer.flatMap(subject => 
                 subject.student.map(student => student._id.toString())
             ));
-
             careerCounts[career.name] = uniqueStudents.size;
-            console.log(`Conteo único de estudiantes en ${career.name}:`, uniqueStudents.size);
         });
-
         res.status(200).json(careerCounts);
     } catch (error) {
-        console.log("Error al procesar datos:", error);
         return res.status(500).json({ message: "Error: " + error });
     }
 };
-
-
-
 
 export const graphics = async (req, res) => {
     try {
         let subjects;
            subjects = await allSubjects();
-        
-
         const gradesCount = {
             '10': 0,
             '9': 0,
@@ -75,7 +62,6 @@ export const graphics = async (req, res) => {
         res.status(404).json({ message: error.message });
     }
 }
-
 
 //FUNCION QUE TRAE EL TOKEN PARA FILTRAR LAS CALIFICACIONES
 async function getToken(req) {
@@ -107,37 +93,6 @@ async function allSubjects() {
     ]);
 
     if (!subjects) {
-        return "No hay calificaciones";
-    }
-    return subjects;
-}
-
-async function studentSubjects(userId) {
-    const subjects = await Subject.aggregate([
-        {
-            $lookup: {
-                from: "users",
-                localField: "student",
-                foreignField: "_id",
-                as: "student",
-            },
-        },
-        {
-            $lookup: {
-                from: "graders",
-                localField: "grade",
-                foreignField: "_id",
-                as: "grade",
-            },
-        },
-        {
-            $match: {
-                "student._id": new mongoose.Types.ObjectId(userId),
-            },
-        },
-    ]);
-
-    if (!subjects || subjects.length === 0) {
         return "No hay calificaciones";
     }
     return subjects;
